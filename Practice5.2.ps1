9b934fc198de90db1fde1892f33de83c623e12d4
Martin Pihlapuu <martin.pihlapuu@voco.ee>
2026-03-24 11:02:27 +0200

    Practice5

---
+++ Practice5.2.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem -Path "C:\Temp\Test"
+
+$file = Get-ChildItem -Path "C:\Temp\Test" | Where-Object {$_.Name -like "*.csv"} | Select-Object Name, Length
+
+$sizeinKB = $file.Length / 1KB
+$sizeinMB = $file.Length / 1MB
+
+Write-Host "`nFileName : $($file.Name)"
+Write-Host "Size in KB : $sizeinKB"
+Write-Host "Size in MB : $sizeinMB"
