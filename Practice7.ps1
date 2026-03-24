2bb305c59e73725135ee835bd5c40fd6eca2f685
Martin Pihlapuu <martin.pihlapuu@voco.ee>
2026-03-24 11:04:45 +0200

    Practice7

---
+++ Practice7.ps1
@@ -0,0 +1,16 @@
+﻿$groups = @("Red","Green","Yellow","Blue")
+$result = @()
+
+for($i = 1; $i -le 20; $i++)
+{
+    $g = Get-Random $groups
+
+    $temp = [PSCustomObject]@{
+        RollNumber = $i
+        Group      = $g
+    }
+
+    $result += $temp
+}
+
+$result
