99697cbbd340bcd3bb001c9831c0640db1e2d85e
Martin Pihlapuu <martin.pihlapuu@voco.ee>
2026-03-24 11:10:18 +0200

    Practice12

---
+++ Practice12.1.ps1
@@ -0,0 +1,14 @@
+﻿function countstart
+{
+    $start = Get-Service | Where-Object {$_.Status -eq "Running"}
+    Write-Host "Total services in running state = $($start.Count)"
+}
+
+function countstop
+{
+    $stop = Get-Service | Where-Object {$_.Status -eq "Stopped"}
+    Write-Host "Total services in stopped state = $($stop.Count)"
+}
+
+countstart
+countstop
