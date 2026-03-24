44888b06d0aadef8c50cbe8f2be02dedba08dbdd
Martin Pihlapuu <martin.pihlapuu@voco.ee>
2026-03-24 11:06:53 +0200

    Practice9

---
+++ Practice9.ps1
@@ -0,0 +1,22 @@
+﻿$ct1 = 0
+
+do
+{
+    $ch = $null
+    $ch = Get-Process | Where-Object {$_.Name -like "note*"}
+
+    if($ch -ne $null)
+    {
+        $ct2 = 2
+        Write-Host "Notepad is running"
+        Start-Sleep -Seconds 1
+        $ct1++
+    }
+    else
+    {
+        $ct2 = 1
+    }
+
+}while($ct2 -ne 1)
+
+$ct1
